ab9fc302e918341c7ced3678c83c421382434fe1
kurse <85976272+kuminho-br@users.noreply.github.com>
2023-04-03 09:34:57 -0300

    Add files via upload

--- unityPractice/mark001_PlayerControl/PlayerTwoScript.cs
+++ unityPractice/mark001_PlayerControl/PlayerTwoScript.cs
@@ -2,25 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerTwoScript : MonoBehaviour
 {
     public float speed;
     public float turnSpeed;
     public float horizontalInput;
     public float forwardInput;
-
+    
     // Update is called once per frame
     void Update()
     {
         //getting inputs from the player
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        //horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical2");
 
         //moves the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         //turns the vehicle
-        transform.Rotate(Vector3.up, horizontalInput * Time.deltaTime * turnSpeed);
-
-
+        //transform.Rotate(Vector3.up, horizontalInput * Time.deltaTime * turnSpeed);
     }
 }
