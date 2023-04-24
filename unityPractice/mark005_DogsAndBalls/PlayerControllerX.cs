5cb8e7837d44a7d36342ba4f16c7532cacbcef31
kurse <85976272+kuminho-br@users.noreply.github.com>
2023-04-24 14:01:42 -0300

    Add files via upload

---
+++ unityPractice/mark005_DogsAndBalls/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float dogCooldown;
+    private float lastDogRunTime = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && ((Time.time-lastDogRunTime) > dogCooldown))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastDogRunTime = Time.time;
+        }
+    }
+}
---
+++ unityPractice/mark005_DogsAndBalls/SpawnManagerX.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+    public float startDelay = 1.0f;
+    private float spawnInterval = 3.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("CallMethods", startDelay, spawnInterval);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // Generate random ball index
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+
+        // instantiate random ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+    }
+    void CallMethods()
+    {
+        // generate random spawn interval
+        spawnInterval = Random.Range(2.0f, 4.0f);
+        Debug.Log(spawnInterval);
+        Invoke("SpawnRandomBall",spawnInterval);
+    }
+}
